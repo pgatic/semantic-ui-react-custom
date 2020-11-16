e840fe7a4566f7e1b7f018bb5c6ec17ac77bde3f
pgatic <pgatic@hotmail.com>
2020-11-16 15:22:21 +0100

    add semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { Header, Segment, Form } from "semantic-ui-react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = () => {
+    console.log("clicked");
+    setTodos([...todos, todo]);
+  };
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setTodo(e.target.value);
+  };
+  console.log(todo);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Segment style={{ maxWidth: "60rem", margin: "auto" }}>
+        <Header color="green" as="h3" textAlign="center">
+          TODO LIST
+        </Header>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder="Enter new todo"
+              name="todo"
+              value={todo}
+              onChange={handleChange}
+            />
+            <Form.Button content="Add to list" />
+          </Form.Group>
+        </Form>
+      </Segment>
     </div>
   );
 }
