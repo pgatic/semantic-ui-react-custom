3b2b714c0f2ad17b5c364b9378cdb49f271367a9
pgatic <pgatic@hotmail.com>
2020-11-21 14:38:13 +0100

    installed and ready to explore

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
-import { Header, Segment, Form } from "semantic-ui-react";
+import { Container, Step } from "semantic-ui-react";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
-
-  const handleSubmit = () => {
-    console.log("clicked");
-    setTodos([...todos, todo]);
-  };
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setTodo(e.target.value);
-  };
-  console.log(todo);
   return (
     <div className="App">
-      <Segment style={{ maxWidth: "60rem", margin: "auto" }}>
-        <Header color="green" as="h3" textAlign="center">
-          TODO LIST
-        </Header>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Input
-              placeholder="Enter new todo"
-              name="todo"
-              value={todo}
-              onChange={handleChange}
-            />
-            <Form.Button content="Add to list" />
-          </Form.Group>
-        </Form>
-      </Segment>
+      <Container style={{ marginBottom: "300px" }}>
+        <Step.Group fluid>
+          <Step
+            icon="plane"
+            title="Shipping"
+            description="Choose your shipping options"
+          />
+          <Step
+            active
+            icon="dollar"
+            title="Billing"
+            description="Enter billing information"
+          />
+          <Step
+            disabled
+            icon="info circle"
+            title="Confirm Order"
+            description="Verify order details"
+          />
+        </Step.Group>
+      </Container>
     </div>
   );
 }
